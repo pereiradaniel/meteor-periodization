f2e554a1f51fed02d7fc61a6e2637a8d1d16ceab
Daniel Pereira <pereiradpg@yahoo.ca>
2015-11-13 16:30:43 -0500

    Chart displayed in organized manner
    Ready for basic styling

--- app/client/templates/weeks_list/weeks_list.js
+++ app/client/templates/weeks_list/weeks_list.js
@@ -8,6 +8,10 @@ Template.WeeksList.events({
 /* WeeksList: Helpers */
 /*****************************************************************************/
 Template.WeeksList.helpers({
+	'week_number': function(target) {
+    var this_week_number = target[0][0].week
+    return this_week_number
+  }
 });
 
 /*****************************************************************************/
